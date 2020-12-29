1cb871eebb18b91c77b210e4a13d5f867cc5f6c1
Rehan Khalil <rehankhalil462@gmail.com>
2020-12-30 02:13:19 +0500

    Monsters Rolodex PWA

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { CardList } from './Components/Card-List/card-list.component';
+import React, { Component } from 'react';
+import { Search } from './Components/SearchBox/search-box.component.jsx';
 
-function App() {
+class App extends Component{
+  constructor() {
+    super();
+   this.state = {
+     monsters: [],
+     searchField: ''
+   };
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response =>response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+
+  render() {
+    const { monsters, searchField}= this.state;
+    const filteredMonsters= monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Monsters Rolodex</h1>
+     <Search placeholder='Search Monsters' handleChange={e=> {this.setState({searchField: e.target.value})}}/>
+      <CardList monsters={filteredMonsters}/>
+   </div>
   );
+}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//import reportWebVitals from './reportWebVitals';
+
+// const name = 'Rehan khalil'
+// const currdate= new Date().toLocaleDateString();
+// const currtime= new Date().toLocaleTimeString();
+// <React.Fragment>
+     //<h1>My name is {name} </h1>,
+     //<p> todays date is {currdate}</p>
+   //  <p> todays time is {currtime}</p> 
+ // </React.Fragment> 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <App/>
+   ,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
